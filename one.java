a9ae87a7a78385c8deec0f2d68952b6cc90576f8
e <847724434@qq.com>
2020-02-14 23:11:19 +0800

    多线程version1(Ps:报文处理存在问题)

--- one.java
+++ one.java
@@ -1,8 +1,35 @@
-import java.io.*;
+﻿import java.io.*;
 import java.net.*;
 
-public class Web1 {
-	public static String contenttype(String filename) {
+class MyUrl{
+	StringBuilder sb;
+	
+	String target;
+	
+	public MyUrl(StringBuilder sb1) {
+		this.sb = sb1;
+	}
+	
+	public void getUrl() {
+		String trans = ""+sb;
+		
+		String[] temp = trans.split(" ");
+		
+		this.target = temp[1].substring(1);
+	}
+}
+
+
+class Myresponse implements Runnable{
+	Socket socket;
+	
+	String errorpage = "404.html";
+	
+	public Myresponse(Socket socket1) {
+		this.socket = socket1;
+	}
+	
+	public static String contentType(String filename) {
 		String type = filename.substring(filename.lastIndexOf(".")+1);
 		if(type == "html") {
 			return "text/html";
@@ -19,21 +46,45 @@ public class Web1 {
 		}
 	}
 	
+	public void response(String filename) {
+		try {
+			PrintStream output = new PrintStream(socket.getOutputStream());
+		
+			FileInputStream fis = new FileInputStream(new File(filename));
+		
+			String repsonsedHead = "HTTP/1.1 200 \r\n" + "Content-Type:"+contentType(filename)+"\r\n" + "\r\n";
+
+			byte[] b = new byte[1024];
+
+			output.write(repsonsedHead.getBytes());
 	
-	public static void main(String[] args) throws IOException {
+			int len;
 
-		ServerSocket server = new ServerSocket(9999);
+			while((len = fis.read(b)) != -1){
+				output.write(b,0,len);
+			}
+	
+			output.write(("MSS Studio").getBytes());
+		
+			fis.close();
+			
+			output.close();
 
-		System.out.println("服务器建立完成");
+			System.err.println("成功响应本次请求\n\n\n");
+		}catch(IOException b) {
+			
+		}
+		
+		
+	}
 
-		while (true) {
-
-			Socket socket = server.accept();
-
-			System.err.println("接收到请求！");
-
-			BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream(),"GBK"));
-
+	@Override
+	public void run(){
+		
+		try {
+			
+			BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			
 			StringBuilder sb = new StringBuilder();
 
 			Integer bufferSize = 1024;
@@ -47,116 +98,51 @@ public class Web1 {
 				sb.append(buffer);
 			}
 			
-			String trans = ""+sb;
-			
-			String[] temp = trans.split(" ");
-			
-			String target = temp[1].substring(1);
-
 			System.err.println("获得的请求报文如下：\n"+sb);
-
-			PrintStream output = new PrintStream(socket.getOutputStream());
 
 			System.out.println("开始响应请求");
 			
-			File a = new File(target);
+			MyUrl url =new MyUrl(sb);
 			
-			File wrongpage = new File("404.html");
+			url.getUrl();
 			
-			try {
-				if(a.isDirectory()) {
-					File[] files = a.listFiles();
+			File a = new File(url.target);
+				
+			if(a.isDirectory()) {
+				File[] files = a.listFiles();
+				
+				for(int m=0;m<files.length;m++) {
 					
-					if(files.length == 0) {
-						FileInputStream fis = new FileInputStream(wrongpage);
-						
-						String repsonsedHead = "HTTP/1.1 200 \r\n" + "Content-Type:text/html\r\n" + "\r\n";
+					this.response(files[m].getName());
 
-						byte[] b = new byte[1024];
-
-						output.write(repsonsedHead.getBytes());
-					
-						int len;
-
-						while((len = fis.read(b)) != -1){
-							output.write(b,0,len);
-						}
-					
-						output.write(("MSS Studio").getBytes());
-						
-						fis.close();
-					}
-					else {
-						for(int m=0;m<files.length;m++) {
-							
-						FileInputStream fis = new FileInputStream(files[m]);
-						
-						String repsonsedHead = "HTTP/1.1 200 \r\n" + "Content-Type:"+contenttype(files[m].getName())+"\r\n" + "\r\n";
-
-						byte[] b = new byte[1024];
-
-						output.write(repsonsedHead.getBytes());
-					
-						int len;
-
-						while((len = fis.read(b)) != -1){
-							output.write(b,0,len);
-						}
-					
-						output.write(("MSS Studio").getBytes());
-						
-						fis.close();
-						}
-					}				
 				}
-				
-				else {
-					FileInputStream fis = new FileInputStream(a);
-				
-					String repsonsedHead = "HTTP/1.1 200 \r\n" + "Content-Type:"+contenttype(a.getName())+"\r\n" + "\r\n";
-
-					byte[] b = new byte[1024];
-
-					output.write(repsonsedHead.getBytes());
-				
-					int len;
-
-					while((len = fis.read(b)) != -1){
-						output.write(b,0,len);
-					}
-				
-					output.write(("MSS Studio").getBytes());
-					
-					fis.close();
-				}
-			}catch(IOException e) {
-				
-				FileInputStream fis = new FileInputStream(wrongpage);
-				
-				String repsonsedHead = "HTTP/1.1 200 \r\n" + "Content-Type:text/html\r\n" + "\r\n";
-
-				byte[] b = new byte[1024];
-
-				output.write(repsonsedHead.getBytes());
-			
-				int len;
-
-				while((len = fis.read(b)) != -1){
-					output.write(b,0,len);
-				}
-			
-				output.write(("MSS Studio").getBytes());
-				
-				fis.close();
-			}catch(ArrayIndexOutOfBoundsException f) {
-				
 			}
-
-			output.close();
-
-			System.err.println("成功响应本次请求\n\n\n");
+			else {
+				this.response(a.getName());
+			}
 			
+		} catch (IOException p) {
+			this.response(this.errorpage);
 		}
+		
+	}
+	
+}
 
+public class Server {
+	public static void main(String[] args) throws IOException {
+		ServerSocket server = new ServerSocket(9999);
+		
+		System.out.println("服务器建立完成");
+		
+		while(true) {
+			Socket socket = server.accept();
+			
+			System.err.println("接收到请求！");
+			
+			Thread t = new Thread(new Myresponse(socket));
+			
+			t.start();
+		}
 	}
 }
